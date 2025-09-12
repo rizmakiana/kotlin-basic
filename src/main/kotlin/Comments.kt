7511333db84d6843acf12c95b8187344eebc4b1f
Rizky Maulana <rizky1517@gmail.com>
2025-09-12 18:18:59 +0700

    add Comments Line intro

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("Hello, Rizky. Welcome to Kotlin");
+    // println("this line is never executed because this is comment");
+
+    /*
+    * println("this line is never executed because this is comment multiple line");
+    * println("Kotlin is cool");
+    */
+}
