3980e0c7bf8f301b3f250b41204b0fcea1d4ea2a
Rizky Maulana <rizky1517@gmail.com>
2025-09-12 19:35:58 +0700

    introduce kotlin do/while loop

---
+++ src/main/kotlin/WhileLoop.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var i  = 0
+
+    println("\nWhile loop")
+    while (i < 10){
+        println("Hallo ke $i")
+        i++
+    }
+
+    println("\nDo while")
+    do {
+        println("Hallo ke $i")
+        i--;
+    } while (i > 0)
+}
