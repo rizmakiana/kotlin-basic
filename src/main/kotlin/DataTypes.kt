5fc300448dd0d4b4097e2e84a9e2a6186d33dcdb
Rizky Maulana <rizky1517@gmail.com>
2025-09-12 18:44:13 +0700

    introduce data types

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val name = "Rizky Maulana"  // string
+    var age = 20                // int
+    val bloodGroup = 'B';       // char
+    val isAlive = true;         // boolean
+    var ipk = 3.74;              // double
+
+    println(name);
+    println(age)
+    println(bloodGroup)
+    println(isAlive)
+    println(ipk)
+}
