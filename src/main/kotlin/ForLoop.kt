1d8bd81617afcc61f966129f94c9e4f9fbd0d520
Rizky Maulana <rizky1517@gmail.com>
2025-09-12 19:50:20 +0700

    introduce for loop in kotlin

---
+++ src/main/kotlin/ForLoop.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val numbers = arrayOf(1,2,3,4,5)
+
+    for (value in numbers){
+        println(value)
+    }
+
+    // there is no traditional for loop like java -> for(initialization; condition; increment)
+}
